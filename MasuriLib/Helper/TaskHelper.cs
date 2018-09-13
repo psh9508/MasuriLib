7d08f7bf5091c13328609318c82cbfde99429e48
박성현 <psh9508@ubcare.co.kr>
2018-09-13 15:43:20 +0900

    Add TaskHelper

---
+++ MasuriLib/Helper/TaskHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasuriLib.Helper
+{
+    public static class TaskHelper
+    {
+        public static Task ContinueWithThorwException(this Task src, Action<Task> action)
+        {
+            return src.ContinueWith(x =>
+            {
+                if (x.Exception != null)
+                    throw new Exception(x.Exception.InnerException?.Message);
+
+                action(x);
+            });
+        }
+
+        public static Task ContinueWithThorwException(this Task src, Action<Task> action, TaskScheduler scheduler)
+        {
+            return src.ContinueWith(x =>
+            {
+                if (x.Exception != null)
+                    throw new Exception(x.Exception.InnerException?.Message);
+
+                action(x);
+            }, scheduler);
+        }
+
+        public static Task ContinueWithThorwException<TResult>(this Task<TResult> src, Action<Task<TResult>> action)
+        {
+            return src.ContinueWith(x =>
+            {
+                if (x.Exception != null)
+                    throw new Exception(x.Exception.InnerException?.Message);
+
+                action(x);
+            });
+        }
+
+        public static Task ContinueWithThorwException<TResult>(this Task<TResult> src, Action<Task<TResult>> action, TaskScheduler scheduler)
+        {
+            return src.ContinueWith(x =>
+            {
+                if (x.Exception != null)
+                    throw new Exception(x.Exception.InnerException?.Message);
+
+                action(x);
+            }, scheduler);
+        }
+    }
+}
